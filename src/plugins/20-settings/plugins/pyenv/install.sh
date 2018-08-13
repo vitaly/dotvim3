a9dddd412ec6760504177a26cad5c67acbfab335
Vitaly Kushner <vitaly@astrails.com>
2018-08-13 12:19:24 +0200

    cleanup

---
+++ src/plugins/20-settings/plugins/pyenv/install.sh
@@ -0,0 +1,2 @@
+ask string python2_env "pyenv environment for python2" vim-python2
+ask string python3_env "pyenv environment for python3" vim-python3
--- src/plugins/20-settings/plugins/pyenv/prompt.sh
+++ src/plugins/20-settings/plugins/pyenv/prompt.sh
@@ -2,9 +2,4 @@ desc <<END
 if you are using different python environments it's best to create 2 separate
 environments, one for python2, and another for python3
 END
-if ask_bool pyenv "Setup pyenv integration?" y; then
-  ask string python2_env "pyenv environment for python2" vim-python2
-  ask string python3_env "pyenv environment for python3" vim-python3
-else
-  false
-fi
+ask_bool pyenv "Setup pyenv integration?" y
