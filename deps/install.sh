af3cd63fff09b18d201e88796fbd21deb70e9293
Vitaly Kushner <vitaly@astrails.com>
2019-01-09 10:05:06 +0100

    ruby gem

--- deps/install.sh
+++ deps/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+if command -v gem > /dev/null; then
+  gem install neovim
+fi
+
 # src/plugins/20-settings/plugins/pyenv/files/deps/install.sh -------------------
 
 source .config
--- src/plugins/00-cleanup/files/deps/install.sh
+++ src/plugins/00-cleanup/files/deps/install.sh
@@ -1,1 +1,5 @@
 #!/bin/bash -e
+
+if command -v gem > /dev/null; then
+  gem install neovim
+fi
