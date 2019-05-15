179a0debf38870002a4f5620f58bcc99dd70c6c5
Vitaly Kushner <vitaly@astrails.com>
2019-05-15 12:37:40 +0200

    quiet pip

--- deps/install.sh
+++ deps/install.sh
@@ -44,11 +44,11 @@ fi
 # PYTHON3
 
 if [ -n "$vim_pip3" ]; then
-  if ! $vim_pip3 show tasklib; then
+  if ! $vim_pip3 show tasklib > /dev/null; then
     (set -x; $vim_pip_sudo $vim_pip3 install git+git://github.com/robgolding63/tasklib@develop)
   fi
 
-  if ! $vim_pip3 show six; then
+  if ! $vim_pip3 show six > /dev/null; then
     (set -x; $vim_pip_sudo $vim_pip3 install six)
   fi
 fi
