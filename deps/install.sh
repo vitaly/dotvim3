cea6915c04ff40c81b8c85135c9a79b95c18d77c
Vitaly Kushner <vitaly@astrails.com>
2020-07-20 17:35:30 +0200

    verbose coc extensions install

--- deps/install.sh
+++ deps/install.sh
@@ -20,4 +20,4 @@ yarn global add $(cat deps/node.txt)
 # src/plugins/40-ide/plugins/10-completion/engine/coc/files/deps/install.sh -------------------
 
 # coc extensions
-vim +"CocInstall -sync $(cat deps/coc-extensions.txt)" "+qa"
+TERM=dumb vim "+echo 'installing COC extensions'" "+CocInstall -sync $(echo `cat deps/coc-extensions.txt`)" "+w ! tee >&2" "+qa!"
--- src/plugins/40-ide/plugins/10-completion/engine/coc/files/deps/install.sh
+++ src/plugins/40-ide/plugins/10-completion/engine/coc/files/deps/install.sh
@@ -1,2 +1,2 @@
 # coc extensions
-vim +"CocInstall -sync $(cat deps/coc-extensions.txt)" "+qa"
+TERM=dumb vim "+echo 'installing COC extensions'" "+CocInstall -sync $(echo `cat deps/coc-extensions.txt`)" "+w ! tee >&2" "+qa!"
