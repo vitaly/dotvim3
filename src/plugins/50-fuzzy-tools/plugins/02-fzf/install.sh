216bbbb3f8ba1b3a62859cc782d92b6fbb13586a
Vitaly Kushner <test@test.com>
2020-11-25 17:11:09 +0100

    ibetter FZF_DEFAULT_COMMAND message

--- src/plugins/50-fuzzy-tools/plugins/02-fzf/install.sh
+++ src/plugins/50-fuzzy-tools/plugins/02-fzf/install.sh
@@ -1,4 +1,11 @@
-if [ -z "$FZF_DEFAULT_COMMAND" -a -n "`which ag`" ]; then
+if [[ -z "${FZF_DEFAULT_COMMAND:-}" ]]; then
+  if [[ -n $(command -v rg) ]]; then
+    FZF_SUGGEST="export FZF_DEFAULT_COMMAND=\"rg --files --hidden --follow -g '!{.git,node_modules}/*' 2> /dev/null\""
+  elif [[ -n $(command -v ag) ]]; then
+    FZF_SUGGEST="export FZF_DEFAULT_COMMAND=\"ag --follow --nocolor --nogroup -g ''\""
+  fi
+
+  if [[ $FZF_SUGGEST ]]; then
   yellow
   cat <<END
 
@@ -6,10 +13,11 @@ if [ -z "$FZF_DEFAULT_COMMAND" -a -n "`which ag`" ]; then
 
 Consider setting FZF_DEFAULT_COMMAND environment variable:
 
-  export FZF_DEFAULT_COMMAND="ag --follow --nocolor --nogroup -g ''"
+  $FZF_SUGGEST
 
 ***********************************************************************
 
 END
-  nc
+    nc
+  fi
 fi
