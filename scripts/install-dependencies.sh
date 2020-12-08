b042a8845c4c001dc74ab07110d8b8e3e10b66f7
Vitaly Kushner <vitaly@astrails.com>
2020-12-08 01:04:45 +0100

    deps

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,11 +2,11 @@
 # src/plugins/20-settings/plugins/interpreters/files/scripts/install-dependencies.sh -------------------
 
 if [[ -z ${DOTVIM_SKIP_RUBY_DEPS:-} ]]; then
-  yarn global add $(cat deps/node.txt)
+  yarn global add $(cat deps/nodejs.txt)
 fi
 
 if [[ -z ${DOTVIM_SKIP_PYTHON_DEPS:-} ]]; then
-  ${DOTVIM_PYTHON_PIP:-pip} install --upgrade -r deps/pip.txt
+  ${DOTVIM_PYTHON_PIP:-pip} install --upgrade -r deps/python.txt
 fi
 
 if [[ -z ${DOTVIM_SKIP_NODEJS_DEPS:-} ]]; then
--- src/plugins/20-settings/plugins/interpreters/files/scripts/install-dependencies.sh
+++ src/plugins/20-settings/plugins/interpreters/files/scripts/install-dependencies.sh
@@ -1,9 +1,9 @@
 if [[ -z ${DOTVIM_SKIP_RUBY_DEPS:-} ]]; then
-  yarn global add $(cat deps/node.txt)
+  yarn global add $(cat deps/nodejs.txt)
 fi
 
 if [[ -z ${DOTVIM_SKIP_PYTHON_DEPS:-} ]]; then
-  ${DOTVIM_PYTHON_PIP:-pip} install --upgrade -r deps/pip.txt
+  ${DOTVIM_PYTHON_PIP:-pip} install --upgrade -r deps/python.txt
 fi
 
 if [[ -z ${DOTVIM_SKIP_NODEJS_DEPS:-} ]]; then
