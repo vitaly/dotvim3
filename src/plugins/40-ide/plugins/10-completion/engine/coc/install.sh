ae58976dc06ab1c965a00559f38a0fce960c8b87
Vitaly Kushner <vitaly@astrails.com>
2021-05-28 19:04:29 +0200

    COC versions
    setting to install edge or release

--- src/plugins/40-ide/plugins/10-completion/engine/coc/install.sh
+++ src/plugins/40-ide/plugins/10-completion/engine/coc/install.sh
@@ -1,3 +1,11 @@
-# linking the coc-settings.json vecase it's a pain to always update it in 2 places
+desc 'what version of COC.NVIM to install:'
+
+enum release '- latest official release'
+enum edge '   - build from latest master branch '
+
+ask enum coc_version 'COC version?' 1
+copy_files "$(this_dir)/versions/${coc_version}"
+
+# linking the coc-settings.json becase it's a pain to always update it in 2 places
 rm -f coc-settings.json
 ln -sfn "$(this_dir)/coc-settings.json"
